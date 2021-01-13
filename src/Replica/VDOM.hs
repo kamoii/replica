c9b90b24cc44540d89ac0f008ee10e88ccfcdfac
kamoii <>
2021-01-14 04:16:24 +0900

    Reformat + add comment

--- src/Replica/VDOM.hs
+++ src/Replica/VDOM.hs
@@ -1,27 +1,27 @@
-{-# LANGUAGE ConstraintKinds     #-}
-{-# LANGUAGE DeriveFunctor       #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module Replica.VDOM
-  ( module Replica.VDOM
-  , module Replica.VDOM.Types
-  , module Replica.VDOM.Diff
-  , module Replica.VDOM.Render
-  ) where
+module Replica.VDOM (
+    module Replica.VDOM,
+    module Replica.VDOM.Types,
+    module Replica.VDOM.Diff,
+    module Replica.VDOM.Render,
+) where
 
-import qualified Data.ByteString            as B
-import qualified Data.FileEmbed             as FE
-import qualified Data.Text                  as T
-import qualified Data.Text.Encoding         as T
-import qualified Data.Map                   as M
+import qualified Data.ByteString as B
+import qualified Data.FileEmbed as FE
+import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
-import           Replica.VDOM.Types         (HTML, VDOM(VNode,VLeaf,VText,VRawText), Attrs, Attr(AText,ABool,AEvent,AMap), DOMEvent)
-import           Replica.VDOM.Diff          (Diff, AttrDiff, diff, patch, diffAttrs, patchAttrs)
-import           Replica.VDOM.Render        (renderHTML)
+import Replica.VDOM.Diff (AttrDiff, Diff, diff, diffAttrs, patch, patchAttrs)
+import Replica.VDOM.Render (renderHTML)
+import Replica.VDOM.Types (Attr (ABool, AEvent, AMap, AText), Attrs, DOMEvent, HTML, VDOM (VLeaf, VNode, VRawText, VText))
 
 t :: T.Text -> T.Text
 t = id
@@ -30,54 +30,70 @@ type Path = [Int]
 
 fireWithAttrs :: Attrs -> T.Text -> DOMEvent -> Maybe (IO ())
 fireWithAttrs attrs evtName evtValue = case M.lookup evtName attrs of
-  Just (AEvent attrEvent) -> Just (attrEvent evtValue)
-  _ -> Nothing
+    Just (AEvent attrEvent) -> Just (attrEvent evtValue)
+    _ -> Nothing
 
+-- Actually, it doens't fire right away since result is `Myabe (IO ())'.
 fireEvent :: HTML -> Path -> T.Text -> DOMEvent -> Maybe (IO ())
-fireEvent _ []      = \_ _ -> Nothing
-fireEvent ds (x:xs) = if x < length ds
-  then fireEventOnNode (ds !! x) xs
-  else \_ _ -> Nothing
+fireEvent _ [] = \_ _ -> Nothing
+fireEvent ds (x : xs) =
+    if x < length ds
+        then fireEventOnNode (ds !! x) xs
+        else \_ _ -> Nothing
   where
-    fireEventOnNode (VNode _ attrs _) []        = fireWithAttrs attrs
-    fireEventOnNode (VLeaf _ attrs) []          = fireWithAttrs attrs
-    fireEventOnNode (VNode _ _ children) (p:ps) = if p < length children
-      then fireEventOnNode (children !! p) ps
-      else \_ _ -> Nothing
-    fireEventOnNode _ _                         = \_ _ -> Nothing
+    fireEventOnNode (VNode _ attrs _) [] = fireWithAttrs attrs
+    fireEventOnNode (VLeaf _ attrs) [] = fireWithAttrs attrs
+    fireEventOnNode (VNode _ _ children) (p : ps) =
+        if p < length children
+            then fireEventOnNode (children !! p) ps
+            else \_ _ -> Nothing
+    fireEventOnNode _ _ = \_ _ -> Nothing
 
 clientDriver :: B.ByteString
 clientDriver = $(FE.embedFile "./js/dist/client.js")
 
 defaultIndex :: T.Text -> HTML -> HTML
 defaultIndex title header =
-  [ VLeaf "meta" (fl [("charset", AText "utf-8")])
-  , VLeaf "!doctype" (fl [("html", ABool True)])
-  , VNode "html" mempty
-      [ VNode "head" mempty ([VNode "title" mempty [VText title]] <> header)
-      , VNode "body" mempty
-          [ VNode "script" (fl [("language", AText "javascript")])
-              [ VRawText $ T.decodeUtf8 clientDriver ]
-          ]
-      ]
-  ]
+    [ VLeaf "meta" (fl [("charset", AText "utf-8")])
+    , VLeaf "!doctype" (fl [("html", ABool True)])
+    , VNode
+        "html"
+        mempty
+        [ VNode "head" mempty ([VNode "title" mempty [VText title]] <> header)
+        , VNode
+            "body"
+            mempty
+            [ VNode
+                "script"
+                (fl [("language", AText "javascript")])
+                [VRawText $ T.decodeUtf8 clientDriver]
+            ]
+        ]
+    ]
   where
     fl = M.fromList
 
 ssrHtml :: T.Text -> T.Text -> HTML -> HTML -> HTML
 ssrHtml title wsPath header body =
-  [ VLeaf "!doctype" (fl [("html", ABool True)])
-  , VNode "html" mempty
-      [ VNode "head" mempty $
-        [ VLeaf "meta" (fl [("charset", AText "utf-8")])
-        , VNode "title" mempty [VText title]
-        ] <> header
-      , VNode "body" (fl [("data-replica-ws-path", AText wsPath)])
-          [ VNode "div" (fl [("data-app", AText "replica")]) body
-          , VNode "script" (fl [("language", AText "javascript")])
-              [ VRawText $ T.decodeUtf8 clientDriver ]
-          ]
-      ]
-  ]
+    [ VLeaf "!doctype" (fl [("html", ABool True)])
+    , VNode
+        "html"
+        mempty
+        [ VNode "head" mempty $
+            [ VLeaf "meta" (fl [("charset", AText "utf-8")])
+            , VNode "title" mempty [VText title]
+            ]
+                <> header
+        , VNode
+            "body"
+            (fl [("data-replica-ws-path", AText wsPath)])
+            [ VNode "div" (fl [("data-app", AText "replica")]) body
+            , VNode
+                "script"
+                (fl [("language", AText "javascript")])
+                [VRawText $ T.decodeUtf8 clientDriver]
+            ]
+        ]
+    ]
   where
     fl = M.fromList
--- src/Replica/VDOM/Types.hs
+++ src/Replica/VDOM/Types.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Replica.VDOM.Types where
 
-import           Data.Aeson                 ((.=))
-import qualified Data.Aeson                 as A
-import qualified Data.Text                  as T
-import qualified Data.Map                   as M
+import Data.Aeson ((.=))
+import qualified Data.Aeson as A
+import qualified Data.Map as M
+import qualified Data.Text as T
 
 t :: T.Text -> T.Text
 t = id
@@ -13,44 +13,49 @@ t = id
 type HTML = [VDOM]
 
 data VDOM
-  = VNode    !T.Text !Attrs ![VDOM]
-  | VLeaf    !T.Text !Attrs
-  | VText    !T.Text
-  | VRawText !T.Text
+    = VNode !T.Text !Attrs ![VDOM]
+    | VLeaf !T.Text !Attrs
+    | VText !T.Text
+    | VRawText !T.Text
 
 instance A.ToJSON VDOM where
-  toJSON (VText text) = A.object
-    [ "type" .= t "text"
-    , "text" .= text
-    ]
-  toJSON (VRawText text) = A.object
-    [ "type" .= t "text"
-    , "text" .= text
-    ]
-  toJSON (VLeaf element attrs) = A.object
-    [ "type"    .= t "leaf"
-    , "element" .= element
-    , "attrs"   .= attrs
-    ]
-  toJSON (VNode element attrs children) = A.object
-    [ "type"     .= t "node"
-    , "element"  .= element
-    , "attrs"    .= attrs
-    , "children" .= children
-    ]
+    toJSON (VText text) =
+        A.object
+            [ "type" .= t "text"
+            , "text" .= text
+            ]
+    toJSON (VRawText text) =
+        A.object
+            [ "type" .= t "text"
+            , "text" .= text
+            ]
+    toJSON (VLeaf element attrs) =
+        A.object
+            [ "type" .= t "leaf"
+            , "element" .= element
+            , "attrs" .= attrs
+            ]
+    toJSON (VNode element attrs children) =
+        A.object
+            [ "type" .= t "node"
+            , "element" .= element
+            , "attrs" .= attrs
+            , "children" .= children
+            ]
 
 type Attrs = M.Map T.Text Attr
 
+-- TODO: Add a note why we need `AMap !Attrs'
 data Attr
-  = AText  !T.Text
-  | ABool  !Bool
-  | AEvent !(DOMEvent -> IO ())
-  | AMap   !Attrs
+    = AText !T.Text
+    | ABool !Bool
+    | AEvent !(DOMEvent -> IO ())
+    | AMap !Attrs
 
 instance A.ToJSON Attr where
-  toJSON (AText v) = A.String v
-  toJSON (ABool v)  = A.Bool v
-  toJSON (AEvent _) = A.Null
-  toJSON (AMap v)   = A.toJSON $ fmap A.toJSON v
+    toJSON (AText v) = A.String v
+    toJSON (ABool v) = A.Bool v
+    toJSON (AEvent _) = A.Null
+    toJSON (AMap v) = A.toJSON $ fmap A.toJSON v
 
-newtype DOMEvent = DOMEvent { getDOMEvent :: A.Value }
+newtype DOMEvent = DOMEvent {getDOMEvent :: A.Value}
